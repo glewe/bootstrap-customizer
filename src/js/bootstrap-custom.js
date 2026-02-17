455f05712f35cdf96b3194930c590049b909a22e
George Lewe <george@lewe.com>
2026-02-17 20:05:32 +0100

    Add color mode switch

--- src/js/bootstrap-custom.js
+++ src/js/bootstrap-custom.js
@@ -3,6 +3,7 @@
  * This includes Popper and all of Bootstrap's JS plugins.
  */
 import '../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'
+import './color-modes.js'
 
 /*
  * Place any custom JS below...
